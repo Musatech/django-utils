261e8fd35c39792393f2c195f8733007b78ee00b
Shinneider Silva <shinneider>
2024-08-19 10:52:34 -0300

    feat: add array filter

---
+++ src/django_filters/filters.py
@@ -0,0 +1,27 @@
+from django_filters.filters import ChoiceFilter, MultipleChoiceFilter
+
+
+class ArrayChoiceMixin:
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('lookup_expr', 'contains')
+        super().__init__(*args, **kwargs)
+
+    def filter(self, qs, value):
+        if not value:  # Even though not a noop, no point filtering if empty.
+            return qs
+
+        if self.is_noop(qs, value):
+            return qs
+
+        value = value if isinstance(value, list) else [value]
+        qs = self.get_method(qs)(**self.get_filter_predicate(value))
+        return qs.distinct() if self.distinct else qs
+
+
+class ArrayChoiceFilter(ChoiceFilter, ArrayChoiceMixin):
+    pass
+
+
+class ArrayMultipleChoiceFilter(MultipleChoiceFilter, ArrayChoiceMixin):
+    pass
