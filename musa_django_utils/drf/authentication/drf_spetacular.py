0d5d30c64e966efd475f20d1ffb3bcc3d274a07f
Shinneider Silva <shinneider>
2024-08-20 17:34:15 -0300

    feat : fix drf-spetacular

--- musa_django_utils/drf/authentication/drf_spetacular.py
+++ musa_django_utils/drf/authentication/drf_spetacular.py
@@ -22,26 +22,25 @@ class BaseRemoteAuthScheme(OpenApiAuthenticationExtension):  # pragma: no cover
             'bearerFormat': 'JWT',
             # 'description': _('Token-based authentication')
         }
-        if provider := self.target.get_provider_header_value():
-            field_name = self.target.get_provider_header()
+        if provider := self.target.get_config('PROVIDER_HEADER_VALUE'):
+            field_name = self.target.get_config('PROVIDER_HEADER', self.target.provider_header)
             data['description'] = _(f'You need to set a `{field_name}` with a value `{provider}` in header')
 
         return data
 
 
-class WmsCookieAuthScheme(OpenApiAuthenticationExtension):  # pragma: no cover
+class WmsCookieAuthScheme(BaseRemoteAuthScheme):  # pragma: no cover
     target_class = 'musa_django_utils.drf.authentication.old_django.OldDjangoCookieSessionAuthentication'
-    name = 'Django Session Cookie Authentication'
-    priority = 0
-
-    def __init__(self, target):
-        super().__init__(target)
-        self.name = getattr(self.target, 'name', self.name)
 
     def get_security_definition(self, auto_schema):
-        return {
+        data = {
             'type': 'apiKey',
             'in': 'cookie',
-            'name': self.target.cookie_name,
-            'description': _('Use WMS Cookie-based session authentication')
+            'name': self.target.get_config('AUTHENTICATION_FIELD', self.target.authentication_field),
+            # 'description': _('Token-based authentication')
         }
+        if provider := self.target.get_config('PROVIDER_HEADER_VALUE'):
+            field_name = self.target.get_config('PROVIDER_HEADER', self.target.provider_header)
+            data['description'] = _(f'You need to set a `{field_name}` with a value `{provider}` in header')
+
+        return data
