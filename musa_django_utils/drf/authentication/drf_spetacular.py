caf573d01763aed05707e17a1cf89ce3d3d8da63
Shinneider <118396271+shinneider-musa@users.noreply.github.com>
2024-12-13 18:13:30 -0500

    Feature/generic oauth (#16)
    * New Generic OAuth Class
    
    * fix Generic OAuth import
    
    * Remove wms token compatibility

--- musa_django_utils/drf/authentication/drf_spetacular.py
+++ musa_django_utils/drf/authentication/drf_spetacular.py
@@ -22,20 +22,3 @@ class BaseRemoteAuthScheme(OpenApiAuthenticationExtension):  # pragma: no cover
             'bearerFormat': 'JWT',
             # 'description': _('Token-based authentication')
         }
-
-
-class WmsCookieAuthScheme(BaseRemoteAuthScheme):  # pragma: no cover
-    target_class = 'musa_django_utils.drf.authentication.old_django.OldDjangoCookieSessionAuthentication'
-
-    def get_security_definition(self, auto_schema):
-        data = {
-            'type': 'apiKey',
-            'in': 'cookie',
-            'name': self.target.get_config('AUTHENTICATION_FIELD', self.target.authentication_field),
-            # 'description': _('Token-based authentication')
-        }
-        if provider := self.target.get_config('PROVIDER_HEADER_VALUE'):
-            field_name = self.target.get_config('PROVIDER_HEADER', self.target.provider_header)
-            data['description'] = _(f'You need to set a `{field_name}` with a value `{provider}` in header')
-
-        return data
