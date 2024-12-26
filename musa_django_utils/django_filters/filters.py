bac454553b188b6bc598d721bd7d37acd0c8ca7d
Shinneider <118396271+shinneider-musa@users.noreply.github.com>
2024-12-25 23:22:20 -0500

    Revert "Feature/new filters (#19)"
    This reverts commit 8aec65f32b8fc2443a6801e9355b870ca208dadb.

--- musa_django_utils/django_filters/filters.py
+++ musa_django_utils/django_filters/filters.py
@@ -1,5 +1,5 @@
 from django_filters.filters import (BaseInFilter, CharFilter, ChoiceFilter, DateFilter, MultipleChoiceFilter,
-                                    NumberFilter, SlugFilter, UUIDFilter)
+                                    NumberFilter, UUIDFilter)
 
 
 class ArrayMixin:
@@ -33,10 +33,6 @@ class NumberInFilter(BaseInFilter, NumberFilter):
 
 
 class CharInFilter(BaseInFilter, CharFilter):
-    pass
-
-
-class SlugInFilter(BaseInFilter, SlugFilter):
     pass
 
 
