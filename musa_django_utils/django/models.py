a629cde5f57511b97f04b5ba1dffe6cb97f27c7e
Shinneider <118396271+shinneider-musa@users.noreply.github.com>
2025-04-25 10:08:33 -0300

    Fix Model.objects.all() showing deleted objects (#26)

--- musa_django_utils/django/models.py
+++ musa_django_utils/django/models.py
@@ -1,19 +1,37 @@
-from django.db.models import Model, QuerySet
+from django.db.models import Manager, Model, QuerySet
 from django.db.models.fields import BooleanField, DateTimeField
 from django.utils import timezone
 
 
+class BaseDeletedManager(Manager):
+
+    def all(self, deleted=False):
+        return self.get_queryset().all(deleted)
+
+
 class BaseDeletedQueryset(QuerySet):
 
-    def all(self):
-        return self.filter(deleted=False)
+    def all(self, deleted=False):
+        if deleted is not None:
+            self._filter_deleted = deleted
+            return self.filter(deleted=deleted)
+        return super().all()
+
+    def filter(self, *args, **kwargs):
+        if hasattr(self, '_filter_deleted'):
+            kwargs['deleted'] = self._filter_deleted
+        return super().filter(*args, **kwargs)
 
     def delete(self):
         self.update(deleted=True, updated_at=timezone.now())
 
-    def filter(self, *args, **kwargs):
-        kwargs['deleted'] = False
-        return super().filter(*args, **kwargs)
+    def as_manager(cls):
+        manager = BaseDeletedManager.from_queryset(cls)()
+        manager._built_with_as_manager = True
+        return manager
+
+    as_manager.queryset_only = True
+    as_manager = classmethod(as_manager)
 
 
 class BaseEnabledQueryset(QuerySet):
